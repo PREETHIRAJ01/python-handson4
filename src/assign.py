cf7971b79712929799e320389c7542fcf1580672
Admin <Admin@10.17.212.29>
2017-12-09 10:51:10 +0530

    my assignments on functions is finished

---
+++ src/assign.py
@@ -0,0 +1,10 @@
+def compare(phoenix_to_slc,phoenix_to_tampa):
+    if phoenix_to_slc>phoenix_to_tampa:
+        print("slc is far from phoenix compared to tampa")
+    elif phoenix_to_slc<phoenix_to_tampa:
+        print("tampa,florida is far from phoenix compared to slc")
+    else:
+        print("both location are same")
+
+
+compare(1750,576)
